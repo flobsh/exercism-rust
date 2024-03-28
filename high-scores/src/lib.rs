b05d06f8aaf47d3e7ef3a01d46cd6de9f8124a17
Florian Bescher <florian.bescher22@gmail.com>
2024-03-28 18:21:35 +0100

    high-scores: add solution

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut scores = self.scores.to_owned();
+        scores.sort_unstable_by(|a, b| b.cmp(a));
+        scores.truncate(3);
+        scores
+    }
+}
