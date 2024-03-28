efe70306ae1590a4cf42cac1888c97f3b93202a9
Florian Bescher <florian.bescher22@gmail.com>
2024-03-28 18:11:40 +0100

    clock: add solution

---
+++ clock/src/lib.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock {
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
